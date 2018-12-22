e9860fbc70a25bc139d082208d439199fe9ef971
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-12-22 22:24:00 +0900

    Add simple repl project

---
+++ repl/src/main/java/org/tinylisp/repl/App.java
@@ -0,0 +1,53 @@
+package org.tinylisp.repl;
+
+import org.tinylisp.engine.Engine;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class App {
+
+    private final Engine mEngine = new Engine();
+    private final Engine.TLEnvironment mEnv = Engine.defaultEnvironment();
+    private final Scanner mScanner;
+    private final PrintStream mOut;
+
+    public App() {
+        this(System.in, System.out);
+    }
+
+    public App(InputStream in, PrintStream out) {
+        mScanner = new Scanner(in);
+        mOut = out;
+    }
+
+    public void start() {
+        while (true) {
+            String input = prompt();
+            if (input != null) {
+                try {
+                    Object result = mEngine.execute(input, mEnv);
+                    mOut.println(result == null ? "" : result);
+                } catch (Exception ex) {
+                    mOut.println(ex);
+                }
+            }
+        }
+    }
+
+    private String prompt() {
+        mOut.print(">>> ");
+        try {
+            return mScanner.nextLine();
+        } catch (NoSuchElementException ex) {
+            System.exit(0);
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        new App().start();
+    }
+}
---
+++ repl/src/test/java/org/tinylisp/repl/AppTest.java
@@ -0,0 +1,9 @@
+package org.tinylisp.repl;
+
+import org.junit.Test;
+
+public class AppTest {
+    @Test public void testInstantiate() {
+        new App();
+    }
+}
