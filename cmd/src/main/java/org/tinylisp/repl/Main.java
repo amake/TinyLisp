d8ed5da481ae00464da93c7c0f955e405ce6bfb6
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-12-31 00:16:06 +0900

    Execute from stdin with tinylisp cmd

--- cmd/src/main/java/org/tinylisp/repl/Main.java
+++ cmd/src/main/java/org/tinylisp/repl/Main.java
@@ -2,6 +2,9 @@ package org.tinylisp.repl;
 
 import org.tinylisp.engine.Engine;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,6 +15,10 @@ public class Main {
     public static Object execute(Path path) throws Exception {
         byte[] bytes = Files.readAllBytes(path);
         String program = new String(bytes, StandardCharsets.UTF_8);
+        return execute(program);
+    }
+
+    public static Object execute(String program) throws Exception {
         Engine engine = new Engine();
         Engine.TLEnvironment env = Engine.defaultEnvironment();
         return engine.execute(program, env);
@@ -21,8 +28,22 @@ public class Main {
         if (args.length > 0) {
             Object result = execute(Paths.get(args[0]));
             System.out.println(result);
+        } else if (System.in.available() > 0) {
+            String program = readString(System.in);
+            Object result = execute(program);
+            System.out.println(result);
         } else {
             new Repl().start();
         }
+    }
+
+    private static String readString(InputStream is) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString(StandardCharsets.UTF_8.name());
     }
 }
