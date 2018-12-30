e57cea120483732f254481dea891c5aa6ba78f1c
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-12-30 23:46:07 +0900

    Execute file argument with tinylisp command

--- cmd/src/main/java/org/tinylisp/repl/Main.java
+++ cmd/src/main/java/org/tinylisp/repl/Main.java
@@ -1,7 +1,28 @@
 package org.tinylisp.repl;
 
+import org.tinylisp.engine.Engine;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
-        new Repl().start();
+
+    public static Object execute(Path path) throws Exception {
+        byte[] bytes = Files.readAllBytes(path);
+        String program = new String(bytes, StandardCharsets.UTF_8);
+        Engine engine = new Engine();
+        Engine.TLEnvironment env = Engine.defaultEnvironment();
+        return engine.execute(program, env);
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length > 0) {
+            Object result = execute(Paths.get(args[0]));
+            System.out.println(result);
+        } else {
+            new Repl().start();
+        }
     }
 }
