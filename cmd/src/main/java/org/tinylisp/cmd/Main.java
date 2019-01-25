1aa75a5fe3ae2d13a95459fafc20c2b68f5018ca
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-01-25 10:52:35 +0900

    Refactor cmd string reading into Util class

--- cmd/src/main/java/org/tinylisp/cmd/Main.java
+++ cmd/src/main/java/org/tinylisp/cmd/Main.java
@@ -2,26 +2,9 @@ package org.tinylisp.cmd;
 
 import org.tinylisp.engine.Engine;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
-
-    public static Object execute(Path path) throws Exception {
-        byte[] bytes = Files.readAllBytes(path);
-        String program = new String(bytes, StandardCharsets.UTF_8);
-        return execute(program);
-    }
-
-    public static Object execute(InputStream stream) throws Exception {
-        String program = readString(stream);
-        return execute(program);
-    }
 
     public static Object execute(String program) throws Exception {
         Engine engine = new Engine();
@@ -31,23 +14,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         if (args.length > 0) {
-            Object result = execute(Paths.get(args[0]));
+            Object result = execute(Util.readString(Paths.get(args[0])));
             System.out.println(result);
         } else if (System.in.available() > 0) {
-            Object result = execute(System.in);
+            Object result = execute(Util.readString(System.in));
             System.out.println(result);
         } else {
             new Repl().start();
         }
-    }
-
-    private static String readString(InputStream is) throws IOException {
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = is.read(buffer)) != -1) {
-            result.write(buffer, 0, length);
-        }
-        return result.toString(StandardCharsets.UTF_8.name());
     }
 }
---
+++ cmd/src/main/java/org/tinylisp/cmd/Util.java
@@ -0,0 +1,25 @@
+package org.tinylisp.cmd;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class Util {
+    static String readString(Path path) throws Exception {
+        byte[] bytes = Files.readAllBytes(path);
+        return new String(bytes, StandardCharsets.UTF_8);
+    }
+
+    static String readString(InputStream is) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = is.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString(StandardCharsets.UTF_8.name());
+    }
+}
