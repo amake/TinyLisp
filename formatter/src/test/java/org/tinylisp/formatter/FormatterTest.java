4c9ed4ff99cec6a6c06134c291d68b1678102a23
Aaron Madlon-Kay <aaron@madlon-kay.com>
2019-01-28 21:36:51 +0900

    Refactor formatter tests

--- formatter/src/test/java/org/tinylisp/formatter/FormatterTest.java
+++ formatter/src/test/java/org/tinylisp/formatter/FormatterTest.java
@@ -1,21 +1,31 @@
 package org.tinylisp.formatter;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FormatterTest {
-    @Test public void testFormat() {
-        Formatter formatter = new Formatter();
+
+    private Formatter formatter;
+
+    @Before public void setUp() {
+        formatter = new Formatter();
+    }
+
+    @Test public void testCustomVisitor() {
         formatter.addVisitor(new Formatter.Visitor() {
-            @Override public void visit(Formatter.TLAggregateToken parent, Formatter.TLToken child, int depth) {
+            @Override
+            public void visit(Formatter.TLAggregateToken parent, Formatter.TLToken child, int depth) {
                 if (child instanceof Formatter.TLAtomToken) {
                     ((Formatter.TLAtomToken) child).value += '!';
                 }
             }
         });
         assertEquals("(!a! !b! !c!)!", formatter.format("(a b c)"));
-        formatter = new Formatter();
+    }
+
+    @Test public void testFormat() {
         assertEquals("(let ((foo (+ 1 1)))\n (bar)\n (baz))", formatter.format("(let ((foo (+ 1 1))) (bar) (baz))"));
         assertEquals("(let ((foo 1)\n      (bar 2))\n (baz)\n (buzz))", formatter.format("(let ((foo 1) (bar 2)) (baz) (buzz))"));
         assertEquals("(let ((foo (+ 1 1)))\n (bar)\n (baz))", formatter.format("(let ((foo (+ 1 1))) (bar) (baz))"));
@@ -26,13 +36,11 @@ public class FormatterTest {
     }
 
     @Test public void testPartialInput() {
-        Formatter formatter = new Formatter();
         assertEquals("(if a\n )", formatter.format("(if a )"));
         assertEquals("(if a", formatter.format("(if a"));
     }
 
     @Test public void testIdempotency() {
-        Formatter formatter = new Formatter();
         assertEquals("(if a\n )", formatter.format("(if a\n )"));
     }
 }
